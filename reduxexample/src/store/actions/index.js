6185e172c888004ae9b2b4a72507ac48b21ee8fd
Aijaz Abbasi <muhammadaijaz76@gmail.com>
2021-03-28 00:39:02 +0500

    thunk middleware added

--- reduxexample/src/store/actions/index.js
+++ reduxexample/src/store/actions/index.js
@@ -14,6 +14,7 @@ export class actions {
 
   static fetchData = () => {
     return dispatch => {
+      dispatch({type:"LOADING"})
       return middleware.fetchData(1).then(data=>{
         dispatch({type:"ADD",payload:data})
       }).catch(error => {
